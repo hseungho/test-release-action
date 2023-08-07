6ffeb0dbe7139bcc6802eae318adbc6caf1aa0ea
hseungho <42243302+hseungho@users.noreply.github.com>
2023-08-08 00:58:30 +0900

    Release 0.0.4 (#9) (#10)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Create HelloGo.go (#7)
    
    * Create Sum.go (#8)
    
    * feat: Add arthemetic func

---
+++ HelloGo.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello Go!")
+
+    fmt.Println(sum(20, 30))
+
+    fmt.Println(subs(20, 10))
+
+    fmt.Println(abs(-1002))
+}
+
+func sum(i, j int) int {
+    return i + j
+}
+
+func subs(i, j int) int {
+    return i - j
+}
+
+func abs(i int) int {
+    if (i < 0) {
+        return -i
+    }
+    return i
+}
