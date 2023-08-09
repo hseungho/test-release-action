0fe4a26ed86ab38e2ba867e8fbfb9ac323de530f
hseungho <42243302+hseungho@users.noreply.github.com>
2023-08-09 01:47:15 +0000

    feat: Release test 2

--- HelloGo.go
+++ HelloGo.go
@@ -7,24 +7,24 @@ import (
 func main() {
 	fmt.Println("Hello Go!")
 
-    fmt.Println(sum(20, 30))
+	fmt.Println(sum(20, 30))
 
-    fmt.Println(subs(20, 10))
+	fmt.Println(subs(20, 10))
 
-    fmt.Println(abs(-1002))
+	fmt.Println(abs(-1002))
 }
 
 func sum(i, j int) int {
-    return i + j
+	return i + j
 }
 
 func subs(i, j int) int {
-    return i - j
+	return i - j
 }
 
 func abs(i int) int {
-    if (i < 0) {
-        return -i
-    }
-    return i
+	if i < 0 {
+		return -i
+	}
+	return i
 }
